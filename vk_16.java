0d4786b4e6a240486374de6f452b60020e2381f2
vigneshk3008 <vigneshcse007@gmail.com>
2017-03-08 12:49:24 +0530

    Create vk_16.java

---
+++ vk_16.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class HelloWorld{
+     public static void main(String []args){
+        Scanner in= new Scanner(System.in);
+        String n1=in.nextLine();
+        char ch[]=new char[n1.length()];
+        int n2[]=new int[n1.length()];
+        for(int i=0;i<n1.length();i++)
+        {
+            ch[i]=n1.charAt(i);
+            n2[i]=(int)ch[i];
+            //System.out.println(n2[i]);
+        }
+        Arrays.sort(ch);
+        //System.out.println(ch[1]);
+        System.out.println("The result is : "+arrFun(ch));
+     }
+     private static char arrFun(char[] ch)
+     {
+         char result='\0';
+         for(int i=0;i<ch.length;i++)
+         {
+             result^=ch[i];
+             System.out.print(result);
+         }return result;
+     }
+}
